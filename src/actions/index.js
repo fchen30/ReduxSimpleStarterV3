90810b10100bcb97b344f78fc0330444beee5d4f
Fan Chen <chenfa@amazon.com>
2018-11-07 17:34:08 -0800

    finished 95%

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,18 @@
 
 import axios from 'axios';
 
-const API_KEY = '';
+const API_KEY = '0d6a975b060f09bdcdc08e41ce720f21';
 const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 // axios only works for requests from web browser,
 export const FETCH_WEATHER='FETCH_WEATHER';
 export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},us`;
-    //returned request is a promise
+    //returned request is a promise, promise does not contain data
     const request = axios.get(url);
     console.log('Request', request);
     return {
         type: FETCH_WEATHER,
+        //return promise with payload key
         payload: request
     }
 }
